66f12119e2c6ab1f16626034b22386d37cc495e4
Christian Blouin <bongotastic@gmail.com>
2021-12-07 16:09:46 -0400

    create the class

--- dreidelSim.py
+++ dreidelSim.py
@@ -1,3 +1,11 @@
 '''DreidelSim is a learning project using simulation. It is not meant to have broad use.
 Author: chloemackay and bongotastic
 '''
+
+class DreidelGame:
+    def __init__(self):
+        # The number of coins on the table
+        self.coinTable = 0
+
+        # Each player's coins
+        self.playerCoins = [5,5,5,5,5]
