e4bc2b6b94c8d546d86057974d3ef0bf60d769a1
Christian Blouin <bongotastic@gmail.com>
2021-12-07 16:24:28 -0400

    start game

--- dreidelSim.py
+++ dreidelSim.py
@@ -9,3 +9,22 @@ class DreidelGame:
 
         # Each player's coins
         self.playerCoins = [5,5,5,5,5]
+
+    def PlayerPutCoinOnTable(self, playerIndex):
+        # Take away one coin
+        self.playerCoins[playerIndex] -= 1
+
+        # Add coin to the table
+        self.coinTable += 1
+
+    def StartGame(self):
+        for i in range(len(self.playerCoins)):
+            self.PlayerPutCoinOnTable(i)
+
+if __name__ == "__main__":
+    # create a game
+    myGame = DreidelGame()
+
+    myGame.StartGame()
+
+    print(myGame)
